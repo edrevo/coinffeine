ea670c32273f9ac93b793416bbdfc4f4d1c64fca
Alvaro Polo <apoloval@gmail.com>
2013-11-17 13:38:55 +0100

    Fix build break caused by offer.proto
    The latest change on protocol code were assuming an interface for Amount
    that changed in another merge. This commit fixes this error and improves
    the way that amounts are represented in protocol buffers.
    
    Some minor nitpicks on `OkpayProcessorTest` imports are fixed as well.

--- bitmarket-common/src/main/java/com/bitwise/bitmarket/common/protocol/protobuf/OfferConversions.java
+++ bitmarket-common/src/main/java/com/bitwise/bitmarket/common/protocol/protobuf/OfferConversions.java
@@ -1,5 +1,6 @@
 package com.bitwise.bitmarket.common.protocol.protobuf;
 
+import java.math.BigDecimal;
 import java.util.Currency;
 
 import com.bitwise.bitmarket.common.PeerConnectionParser;
@@ -17,7 +18,7 @@ public class OfferConversions {
                 fromProtobuf(proto.getType()),
                 new PeerId(proto.getFrom()),
                 PeerConnectionParser.parse(proto.getConnection()),
-                new Amount(proto.getAmount(), Currency.getInstance("EUR"))); // TODO: fix this
+                fromProtobuf(proto.getAmount()));
     }
 
     public static OfferType fromProtobuf(OfferProtocol.OfferType offerType) {
@@ -33,13 +34,19 @@ public class OfferConversions {
         }
     }
 
+    public static Amount fromProtobuf(OfferProtocol.Amount amount) {
+        return new Amount(
+                BigDecimal.valueOf(amount.getValue(), amount.getScale()),
+                Currency.getInstance(amount.getCurrency()));
+    }
+
     public static OfferProtocol.PublishOffer toProtobuf(Offer offer) {
         return OfferProtocol.PublishOffer.newBuilder()
                 .setId(offer.getId().getBytes())
                 .setType(toProtobuf(offer.getOfferType()))
                 .setFrom(offer.getFromId().getAddress())
                 .setConnection(offer.getFromConnection().toString())
-                .setAmount(Math.round(offer.getAmount().getAmount())) // TODO: fix this
+                .setAmount(toProtobuf(offer.getAmount()))
                 .build();
     }
 
@@ -52,6 +59,14 @@ public class OfferConversions {
                         "no conversion defined of offer type %s from internal representation to protobuf",
                         offerType.toString()));
         }
+    }
+
+    public static OfferProtocol.Amount toProtobuf(Amount amount) {
+        return OfferProtocol.Amount.newBuilder()
+                .setValue(amount.getAmount().unscaledValue().longValue())
+                .setScale(amount.getAmount().scale())
+                .setCurrency(amount.getCurrency().getSymbol())
+                .build();
     }
 
     private OfferConversions() {}
