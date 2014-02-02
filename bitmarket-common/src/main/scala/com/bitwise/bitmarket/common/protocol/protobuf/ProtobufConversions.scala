014f65d3d1b7c4174186c5e9a4104bcd417e38dd
Sebastian Ortega <sortega@tid.es>
2014-02-03 00:11:01 +0100

    Style fixes in ProtobufConversions

--- bitmarket-common/src/main/scala/com/bitwise/bitmarket/common/protocol/protobuf/ProtobufConversions.scala
+++ bitmarket-common/src/main/scala/com/bitwise/bitmarket/common/protocol/protobuf/ProtobufConversions.scala
@@ -7,7 +7,6 @@ import com.google.protobuf.ByteString
 import com.google.bitcoin.core.{Transaction, Sha256Hash}
 import com.google.bitcoin.crypto.TransactionSignature
 
-import com.bitwise.bitmarket.common.{protocol, PeerConnection}
 import com.bitwise.bitmarket.common.protocol._
 import com.bitwise.bitmarket.common.protocol.protobuf.{BitmarketProtobuf => msg}
 import com.bitwise.bitmarket.common.protocol.protobuf.DefaultProtoMappings._
@@ -38,23 +37,23 @@ object ProtobufConversions {
   )
 
   def fromProtobuf(refundTxSignatureRequest: msg.RefundTxSignatureRequestOrBuilder):
-    protocol.RefundTxSignatureRequest = RefundTxSignatureRequest(
+    RefundTxSignatureRequest = RefundTxSignatureRequest(
       refundTx = fromByteArray(refundTxSignatureRequest.getRefundTx.toByteArray).asInstanceOf[Transaction] ,
       exchangeId = refundTxSignatureRequest.getExchangeId
   )
 
-  def fromProtobuf(enterExchange: msg.EnterExchangeOrBuilder):
-  protocol.EnterExchange = EnterExchange(
+  def fromProtobuf(enterExchange: msg.EnterExchangeOrBuilder): EnterExchange = EnterExchange(
     commitmentTransaction = fromByteArray(enterExchange.getCommitmentTransaction.toByteArray).asInstanceOf[Transaction],
     exchangeId = enterExchange.getExchangeId
   )
 
-  def fromProtobuf(refundTxSignatureResponse: msg.RefundTxSignatureResponseOrBuilder):
-    protocol.RefundTxSignatureResponse = RefundTxSignatureResponse(
-    exchangeId = refundTxSignatureResponse.getExchangeId,
-    refundSignature = TransactionSignature.decodeFromBitcoin(
-      refundTxSignatureResponse.getTransactionSignature.toByteArray, false)
-  )
+  def fromProtobuf(
+      refundTxSignatureResponse: msg.RefundTxSignatureResponseOrBuilder): RefundTxSignatureResponse =
+    RefundTxSignatureResponse(
+      exchangeId = refundTxSignatureResponse.getExchangeId,
+      refundSignature = TransactionSignature.decodeFromBitcoin(
+        refundTxSignatureResponse.getTransactionSignature.toByteArray, false)
+    )
 
   def fromProtobuf(quoteRequest: msg.QuoteRequestOrBuilder): QuoteRequest =
     QuoteRequest(Currency.getInstance(quoteRequest.getCurrency))
@@ -74,9 +73,9 @@ object ProtobufConversions {
     .build
 
   def toProtobuf(quoteRequest: QuoteRequest): msg.QuoteRequest = {
-    val builder = msg.QuoteRequest.newBuilder
-    builder.setCurrency(quoteRequest.currency.getSymbol)
-    builder.build
+    msg.QuoteRequest.newBuilder
+      .setCurrency(quoteRequest.currency.getSymbol)
+      .build
   }
 
   def toProtobuf(quote: Quote): msg.Quote = {
@@ -88,55 +87,46 @@ object ProtobufConversions {
     builder.build
   }
 
-  def toProtobuf(crossNotification: CrossNotification): msg.CrossNotification = {
-    val builder = msg.CrossNotification.newBuilder()
-    builder.setExchangeId(crossNotification.exchangeId)
-    builder.setCross(ProtoMapping.toProtobuf(crossNotification.cross))
-    builder.build
-  }
+  def toProtobuf(crossNotification: CrossNotification): msg.CrossNotification =
+    msg.CrossNotification.newBuilder
+      .setExchangeId(crossNotification.exchangeId)
+      .setCross(ProtoMapping.toProtobuf(crossNotification.cross))
+      .build
 
-  def toProtobuf(exchangeAborted: ExchangeAborted): msg.ExchangeAborted = {
-    val builder = msg.ExchangeAborted.newBuilder()
-    builder.setExchangeId(exchangeAborted.exchangeId)
-    builder.setReason(exchangeAborted.reason)
-    builder.build
-  }
+  def toProtobuf(exchangeAborted: ExchangeAborted): msg.ExchangeAborted =
+    msg.ExchangeAborted.newBuilder
+      .setExchangeId(exchangeAborted.exchangeId)
+      .setReason(exchangeAborted.reason)
+      .build
 
-  def toProtobuf(rejectExchange: protocol.RejectExchange): msg.RejectExchange = {
-    val builder = msg.RejectExchange.newBuilder()
-    builder.setExchangeId(rejectExchange.exchangeId)
-    builder.setReason(rejectExchange.reason)
-    builder.build
-  }
+  def toProtobuf(rejectExchange: RejectExchange): msg.RejectExchange = msg.RejectExchange.newBuilder
+    .setExchangeId(rejectExchange.exchangeId)
+    .setReason(rejectExchange.reason)
+    .build
 
-  def toProtobuf(commitmentNotification: CommitmentNotification): msg.CommitmentNotification = {
-    val builder = msg.CommitmentNotification.newBuilder()
-    builder.setExchangeId(commitmentNotification.exchangeId)
-    builder.setBuyerTxId(ByteString.copyFrom(commitmentNotification.buyerTxId.getBytes))
-    builder.setSellerTxId(ByteString.copyFrom(commitmentNotification.sellerTxId.getBytes))
-    builder.build
-  }
+  def toProtobuf(commitmentNotification: CommitmentNotification): msg.CommitmentNotification =
+    msg.CommitmentNotification.newBuilder
+      .setExchangeId(commitmentNotification.exchangeId)
+      .setBuyerTxId(ByteString.copyFrom(commitmentNotification.buyerTxId.getBytes))
+      .setSellerTxId(ByteString.copyFrom(commitmentNotification.sellerTxId.getBytes))
+      .build
 
-  def toProtobuf(refundTxSignatureRequest: RefundTxSignatureRequest): msg.RefundTxSignatureRequest = {
-    val builder = msg.RefundTxSignatureRequest.newBuilder()
-    builder.setExchangeId(refundTxSignatureRequest.exchangeId)
-    builder.setRefundTx(ByteString.copyFrom(toByteArray(refundTxSignatureRequest.refundTx)))
-    builder.build
-  }
+  def toProtobuf(refundTxSignatureRequest: RefundTxSignatureRequest): msg.RefundTxSignatureRequest =
+    msg.RefundTxSignatureRequest.newBuilder
+      .setExchangeId(refundTxSignatureRequest.exchangeId)
+      .setRefundTx(ByteString.copyFrom(toByteArray(refundTxSignatureRequest.refundTx)))
+      .build
 
-  def toProtobuf(refundTxSignatureResponse: RefundTxSignatureResponse): msg.RefundTxSignatureResponse = {
-    val builder = msg.RefundTxSignatureResponse.newBuilder()
-    builder.setExchangeId(refundTxSignatureResponse.exchangeId)
-    builder.setTransactionSignature(ByteString.copyFrom(refundTxSignatureResponse.refundSignature.encodeToBitcoin()))
-    builder.build()
-  }
+  def toProtobuf(refundTxSignatureResponse: RefundTxSignatureResponse): msg.RefundTxSignatureResponse =
+    msg.RefundTxSignatureResponse.newBuilder
+      .setExchangeId(refundTxSignatureResponse.exchangeId)
+      .setTransactionSignature(ByteString.copyFrom(refundTxSignatureResponse.refundSignature.encodeToBitcoin()))
+      .build()
 
-  def toProtobuf(enterExchange: EnterExchange): msg.EnterExchangeOrBuilder = {
-    val builder = msg.EnterExchange.newBuilder()
-    builder.setExchangeId(enterExchange.exchangeId)
-    builder.setCommitmentTransaction(ByteString.copyFrom(toByteArray(enterExchange.commitmentTransaction)))
-    builder.build
-  }
+  def toProtobuf(enterExchange: EnterExchange): msg.EnterExchangeOrBuilder = msg.EnterExchange.newBuilder
+    .setExchangeId(enterExchange.exchangeId)
+    .setCommitmentTransaction(ByteString.copyFrom(toByteArray(enterExchange.commitmentTransaction)))
+    .build
 
   private def toByteArray(obj: AnyRef): Array[Byte] = {
     val byteArrayOutputStream = new ByteArrayOutputStream
