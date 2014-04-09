da5df3c9759e416a06f5ea6b390782cb24b78104
Ximo Guanter Gonzalbez <ximo@tid.es>
2014-04-09 18:36:33 +0200

    Rename messages

--- coinffeine-client/src/main/scala/com/coinffeine/client/exchange/BuyerExchangeActor.scala
+++ coinffeine-client/src/main/scala/com/coinffeine/client/exchange/BuyerExchangeActor.scala
@@ -6,7 +6,7 @@ import com.coinffeine.client.{ExchangeInfo, MessageForwarding}
 import com.coinffeine.client.exchange.ExchangeActor.{ExchangeSuccess, StartExchange}
 import com.coinffeine.common.protocol.ProtocolConstants
 import com.coinffeine.common.protocol.gateway.MessageGateway.{ReceiveMessage, Subscribe}
-import com.coinffeine.common.protocol.messages.exchange.{OfferSignature, OfferTransaction, PaymentProof}
+import com.coinffeine.common.protocol.messages.exchange.{OfferAccepted, NewOffer, PaymentProof}
 
 /** This actor implements the buyer's side of the exchange. You can find more information about
   * the algorithm at https://github.com/Coinffeine/coinffeine/wiki/Exchange-algorithm
@@ -32,15 +32,15 @@ class BuyerExchangeActor(exchange: Exchange, constants: ProtocolConstants)
     }
 
     private def subscribeToMessages(): Unit = messageGateway ! Subscribe {
-      case ReceiveMessage(OfferSignature(exchangeInfo.`id`, _), exchangeInfo.`counterpart`) => true
+      case ReceiveMessage(OfferAccepted(exchangeInfo.`id`, _), exchangeInfo.`counterpart`) => true
       case _ => false
     }
 
     private def notifyInitialOffer(): Unit =
-      forwardToCounterpart(OfferTransaction(exchangeInfo.id, exchange.getOffer(1)))
+      forwardToCounterpart(NewOffer(exchangeInfo.id, exchange.getOffer(1)))
 
     private val waitForSignedFinalOffer: Receive = {
-      case ReceiveMessage(OfferSignature(_, signature), _)
+      case ReceiveMessage(OfferAccepted(_, signature), _)
         if exchange.validateFinalSignature(signature) =>
         log.info(s"Exchange ${exchangeInfo.id}: exchange finished with success")
         // TODO: Publish transaction to blockchain
@@ -49,19 +49,19 @@ class BuyerExchangeActor(exchange: Exchange, constants: ProtocolConstants)
     }
 
     private def waitForSignedOffer(step: Int): Receive = {
-      case ReceiveMessage(OfferSignature(_, signature), _)
+      case ReceiveMessage(OfferAccepted(_, signature), _)
         if exchange.validateSignature(step, signature) =>
         import context.dispatcher
         forwardToCounterpart(
           exchange.pay(step).map(payment => PaymentProof(exchangeInfo.id, payment.id)))
         if (step == exchangeInfo.steps) {
-          forwardToCounterpart(OfferTransaction(exchangeInfo.id, exchange.finalOffer))
+          forwardToCounterpart(NewOffer(exchangeInfo.id, exchange.finalOffer))
           context.become(waitForSignedFinalOffer)
         } else {
-          forwardToCounterpart(OfferTransaction(exchangeInfo.id, exchange.getOffer(step + 1)))
+          forwardToCounterpart(NewOffer(exchangeInfo.id, exchange.getOffer(step + 1)))
           context.become(waitForSignedOffer(step + 1))
         }
-      case ReceiveMessage(OfferSignature(_, signature), _) =>
+      case ReceiveMessage(OfferAccepted(_, signature), _) =>
         log.warning("OfferAccepted message received with invalid signature. " +
           s"Step: $step, Signature: ${signature.encodeToBitcoin.toSeq} ")
     }
--- coinffeine-common/src/main/scala/com/coinffeine/common/protocol/messages/exchange/OfferAccepted.scala
+++ coinffeine-common/src/main/scala/com/coinffeine/common/protocol/messages/exchange/OfferAccepted.scala
@@ -5,11 +5,11 @@ import com.google.bitcoin.crypto.TransactionSignature
 import com.coinffeine.common.protocol.messages.PublicMessage
 import com.coinffeine.common.protocol.TransactionSignatureUtils
 
-case class OfferSignature(exchangeId: String, signature: TransactionSignature)
+case class OfferAccepted(exchangeId: String, signature: TransactionSignature)
   extends PublicMessage {
 
   override def equals(that: Any) = that match {
-    case offerSignature: OfferSignature => (offerSignature.exchangeId == exchangeId) &&
+    case offerSignature: OfferAccepted => (offerSignature.exchangeId == exchangeId) &&
       TransactionSignatureUtils.equals(offerSignature.signature, signature)
     case _ => false
   }
