45285621ba5ebdebbd9fcfe1207059724c182413
Sebastian Ortega <sortega@tid.es>
2014-06-05 16:02:39 +0200

    Introduce OrderSet.addOrder

--- coinffeine-common/src/main/scala/com/coinffeine/common/protocol/messages/brokerage/OrderSet.scala
+++ coinffeine-common/src/main/scala/com/coinffeine/common/protocol/messages/brokerage/OrderSet.scala
@@ -2,6 +2,7 @@ package com.coinffeine.common.protocol.messages.brokerage
 
 import com.coinffeine.common.currency.{BtcAmount, FiatAmount}
 import com.coinffeine.common.protocol.messages.PublicMessage
+import com.coinffeine.common.protocol.messages.brokerage.OrderSet.Entry
 
 /** Represents the set of orders placed by a peer.
   *
@@ -11,14 +12,19 @@ import com.coinffeine.common.protocol.messages.PublicMessage
   */
 case class OrderSet(
     market: Market,
-    bids: Seq[OrderSet.Entry] = Seq.empty,
-    asks: Seq[OrderSet.Entry] = Seq.empty) extends PublicMessage {
+    bids: Seq[Entry] = Seq.empty,
+    asks: Seq[Entry] = Seq.empty) extends PublicMessage {
 
   requireSingleCurrency()
   requireNotCrossed()
 
   def highestBid: Option[FiatAmount] = bids.map(_.price).reduceOption(_ max _)
   def lowestAsk: Option[FiatAmount] = asks.map(_.price).reduceOption(_ min _)
+
+  def addOrder(orderType: OrderType, amount: BtcAmount, price: FiatAmount) = orderType match {
+    case Bid => copy(bids = addEntry(bids, Entry(amount, price)))
+    case Ask => copy(asks = addEntry(asks, Entry(amount, price)))
+  }
 
   private def requireSingleCurrency(): Unit = {
     val currenciesInOrders = (bids ++ asks).map(_.price.currency).toSet
@@ -31,6 +37,12 @@ case class OrderSet(
       case _ => false
     }
     require(!priceCrossed, "Bids and asks are crossed")
+  }
+
+  private def addEntry(entries: Seq[Entry], entry: Entry): Seq[Entry] = {
+    val (samePrice, otherPrices) = entries.span(_.price == entry.price)
+    val newAmount = (entry +: samePrice).map(_.amount).sum
+    otherPrices :+ Entry(newAmount, entry.price)
   }
 }
 
