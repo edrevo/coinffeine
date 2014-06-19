f7d18a8c07dcdf19eec9c14a71f421f5592b8507
Ximo Guanter <ximo.guanter@gmail.com>
2014-06-19 18:08:08 +0200

    Ensure Akka test time factor also affects Scalatest

--- coinffeine-common-test/src/main/scala/com/coinffeine/common/AkkaSpec.scala
+++ coinffeine-common-test/src/main/scala/com/coinffeine/common/AkkaSpec.scala
@@ -1,14 +1,18 @@
 package com.coinffeine.common
 
 import akka.actor._
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.testkit.{TestKitExtension, ImplicitSender, TestKit}
 import com.typesafe.config.ConfigFactory
 import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
 
 /** FlatSpec configured to test Akka actors. */
 abstract class AkkaSpec(actorSystem: ActorSystem = ActorSystem("TestSystem"))
   extends TestKit(actorSystem) with FlatSpecLike with BeforeAndAfterAll with ShouldMatchers
-  with ImplicitSender {
+  with ImplicitSender with ScalaFutures {
+
+  override def spanScaleFactor: Double =
+    TestKitExtension.get(system).TestTimeFactor
 
   def this(systemName: String) = this(ActorSystem(systemName))
 
