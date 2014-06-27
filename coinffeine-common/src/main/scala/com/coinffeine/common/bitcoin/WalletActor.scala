8d466c96766eec38947e1afdcb58fb86d376907c
Alvaro Polo <apoloval@gmail.com>
2014-06-27 09:44:15 +0200

    Add a wallet actor class hierarchy
    The wallet actor is responsible to wrap a wallet and operate on its
    funds. Under requirement, it will create a multisign transaction and
    will block its founds so they are not used in any other operation.
    The resulting tx can be broadcast so its funds would be spent, or
    it can be released by the wallet actor under demand, turning its funds
    available again.

---
+++ coinffeine-common/src/main/scala/com/coinffeine/common/bitcoin/WalletActor.scala
@@ -0,0 +1,43 @@
+package com.coinffeine.common.bitcoin
+
+import akka.actor.Props
+import com.coinffeine.common.BitcoinAmount
+
+object WalletActor {
+
+  /** A message sent to the wallet actor in order to block some funds in a multisign transaction.
+    *
+    * This message requests some funds to be blocked by the wallet. This will produce a new
+    * transaction included in a FundsBlocked reply message, or FundsBlockingError if something
+    * goes wrong. The resulting transaction can be safely sent to the blockchain with the guarantee
+    * that the outputs it spends are not used in any other transaction. If the transaction is not
+    * finally broadcasted to the blockchain, the funds can be unblocked by sending a ReleasedFunds
+    * message.
+    *
+    * @param requiredSignatures The signatures required to spend the tx in a multisign script
+    * @param amount             The amount of bitcoins to be blocked and included in the transaction
+    */
+  case class BlockFundsInMultisign(requiredSignatures: Seq[KeyPair], amount: BitcoinAmount)
+
+  /** A message sent by the wallet actor in reply to a BlockFundsInMultisign message to report
+    * a successful funds blocking.
+    *
+    * @param request  The request this message is replying to
+    * @param tx       The resulting transaction that contains the funds that have been blocked
+    */
+  case class FundsBlocked(request: BlockFundsInMultisign, tx: MutableTransaction)
+
+  /** A message sent by the wallet actor in reply to a BlockFundsInMultisign message to report
+    * an error while blocking the funds.
+    */
+  case class FundsBlockingError(request: BlockFundsInMultisign, error: Throwable)
+
+  /** A message sent to the wallet actor in order to release the funds that are blocked by the
+    * given transaction.
+    */
+  case class ReleaseFunds(tx: MutableTransaction)
+
+  trait Component {
+    def walletActorProps(wallet: Wallet): Props
+  }
+}
---
+++ coinffeine-common/src/test/scala/com/coinffeine/common/bitcoin/DefaultWalletActorTest.scala
@@ -0,0 +1,46 @@
+package com.coinffeine.common.bitcoin
+
+import akka.actor.Props
+import org.scalatest.concurrent.Eventually
+
+import com.coinffeine.common.{AkkaSpec, BitcoinjTest}
+import com.coinffeine.common.Currency.Implicits._
+import com.coinffeine.common.bitcoin.Implicits._
+
+class DefaultWalletActorTest extends AkkaSpec("DefaultWalletActorTest")
+    with BitcoinjTest with Eventually {
+
+  "Default wallet actor" must "block funds in a multisign transaction" in new Fixture {
+    val request = WalletActor.BlockFundsInMultisign(Seq(keyPair, otherKeyPair), 1.BTC)
+    instance ! request
+    expectMsgPF() {
+      case WalletActor.FundsBlocked(`request`, tx) => wallet.value(tx) should be (-1.BTC)
+    }
+  }
+
+  it must "fail to block funds when there is no enough amount" in new Fixture {
+    val request = WalletActor.BlockFundsInMultisign(Seq(keyPair, otherKeyPair), 10000.BTC)
+    instance ! request
+    expectMsgPF() {
+      case WalletActor.FundsBlockingError(`request`, _: IllegalArgumentException) =>
+    }
+  }
+
+  it must "release blocked funds" in new Fixture {
+    val request = WalletActor.BlockFundsInMultisign(Seq(keyPair, otherKeyPair), 1.BTC)
+    instance ! request
+    val reply = expectMsgType[WalletActor.FundsBlocked]
+    instance ! WalletActor.ReleaseFunds(reply.tx)
+    eventually {
+      wallet.balance() should be(initialFunds)
+    }
+  }
+
+  trait Fixture {
+    val keyPair = new KeyPair
+    val otherKeyPair = new KeyPair
+    val wallet = createWallet(keyPair, 10.BTC)
+    val initialFunds = wallet.balance()
+    val instance = system.actorOf(Props(new DefaultWalletActor(wallet)))
+  }
+}
