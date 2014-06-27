e6b06ffdbc62ee94830fef83f377313801403dff
Alvaro Polo <apoloval@gmail.com>
2014-06-27 09:27:48 +0200

    Add more operations to `PimpMyWallet` class
    More functions are added to block funds based on mutable transactions
    and multisign funds. This means a new pimp-my-library pattern
    for `MutableTransaction` is added.
    
    The unit tests are improved with two additional cases.

--- coinffeine-common/src/main/scala/com/coinffeine/common/bitcoin/Implicits.scala
+++ coinffeine-common/src/main/scala/com/coinffeine/common/bitcoin/Implicits.scala
@@ -1,14 +1,41 @@
 package com.coinffeine.common.bitcoin
 
+import java.math.BigInteger
 import scala.collection.JavaConversions._
 
 import com.google.bitcoin.core.Transaction
+import com.google.bitcoin.core.Transaction.SigHash
+import com.google.bitcoin.script.ScriptBuilder
 import com.google.bitcoin.wallet.WalletTransaction
 
 import com.coinffeine.common._
 import com.coinffeine.common.Currency.Implicits._
 
 object Implicits {
+
+  implicit class PimpMyMutableTransaction(val tx: MutableTransaction) extends AnyVal {
+
+    def addChangeOutput(inputAmount: BitcoinAmount,
+                        spentAmount: BitcoinAmount,
+                        changeAddress: Address): Unit = {
+      val changeAmount = inputAmount - spentAmount
+      require(!changeAmount.isNegative)
+      if (changeAmount.isPositive) {
+        tx.addOutput((inputAmount - spentAmount).asSatoshi, changeAddress)
+      }
+    }
+
+    def addMultisignOutput(amount: BitcoinAmount, requiredSignatures: Seq[PublicKey]): Unit = {
+      require(requiredSignatures.size > 1, "should have at least two signatures")
+      tx.addOutput(
+        amount.asSatoshi,
+        ScriptBuilder.createMultiSigOutputScript(requiredSignatures.size, requiredSignatures)
+      )
+    }
+
+    def outputAmount: BitcoinAmount = Currency.Bitcoin.fromSatoshi(
+      tx.getOutputs.foldLeft(BigInteger.ZERO)((a, b) => a.add(b.getValue)))
+  }
 
   implicit class PimpMyWallet(val wallet: Wallet) extends AnyVal {
 
@@ -23,9 +50,31 @@ object Implicits {
 
     def balance(): BitcoinAmount = Currency.Bitcoin.fromSatoshi(wallet.getBalance)
 
+    def blockFunds(tx: MutableTransaction): Unit = {
+      wallet.commitTx(tx)
+    }
+
     def blockFunds(to: Address, amount: BitcoinAmount): MutableTransaction = {
       val tx = wallet.createSend(to, amount.asSatoshi)
-      wallet.commitTx(tx)
+      blockFunds(tx)
+      tx
+    }
+
+    def blockMultisignFunds(requiredSignatures: Seq[PublicKey],
+                            amount: BitcoinAmount): MutableTransaction = {
+      require(amount.isPositive, s"Amount to block must be greater than zero ($amount given)")
+
+      val inputFunds = collectFunds(amount)
+      val totalInputFunds = valueOf(inputFunds)
+      require(totalInputFunds >= amount,
+        "Input funds must cover the amount of funds to commit")
+
+      val tx = new MutableTransaction(wallet.getNetworkParameters)
+      inputFunds.foreach(tx.addInput)
+      tx.addMultisignOutput(amount, requiredSignatures)
+      tx.addChangeOutput(totalInputFunds, amount, wallet.getChangeAddress)
+      tx.signInputs(SigHash.ALL, wallet)
+      blockFunds(tx)
       tx
     }
 
@@ -42,8 +91,20 @@ object Implicits {
       moveToPool(tx, WalletTransaction.Pool.DEAD)
     }
 
-    private def contains(tx: Transaction): Boolean =
-      Option(wallet.getTransaction(tx.getHash)).isDefined
+    def collectFunds(amount: BitcoinAmount): Set[MutableTransactionOutput] = {
+      val inputFundCandidates = wallet.calculateAllSpendCandidates(true)
+      val necessaryInputCount = inputFundCandidates.view
+        .scanLeft(Currency.Bitcoin.Zero)((accum, output) =>
+        accum + Currency.Bitcoin.fromSatoshi(output.getValue))
+        .takeWhile(_ < amount)
+        .length
+      inputFundCandidates.take(necessaryInputCount).toSet
+    }
+
+    def contains(tx: Transaction): Boolean = Option(wallet.getTransaction(tx.getHash)).isDefined
+
+    private def valueOf(outputs: Traversable[MutableTransactionOutput]): BitcoinAmount =
+      outputs.map(funds => Currency.Bitcoin.fromSatoshi(funds.getValue)).reduce(_ + _)
 
     private def moveToPool(tx: Transaction, pool: WalletTransaction.Pool): Unit = {
       val wtxs = wallet.getWalletTransactions
--- coinffeine-common/src/test/scala/com/coinffeine/common/bitcoin/PimpMyWalletTest.scala
+++ coinffeine-common/src/test/scala/com/coinffeine/common/bitcoin/PimpMyWalletTest.scala
@@ -1,5 +1,7 @@
 package com.coinffeine.common.bitcoin
 
+import scala.util.Random
+
 import com.coinffeine.common.{BitcoinjTest, UnitTest}
 import com.coinffeine.common.Currency.Implicits._
 import com.coinffeine.common.bitcoin.Implicits._
@@ -7,27 +9,67 @@ import com.coinffeine.common.bitcoin.Implicits._
 class PimpMyWalletTest extends UnitTest with BitcoinjTest {
 
   "Pimp my wallet" must "block funds on create send" in new Fixture {
-    val tx = instance.blockFunds(someoneElse, 1.BTC)
+    val tx = instance.blockFunds(someoneElseAddress, 1.BTC)
+    instance.value(tx) should be (-1.BTC)
+    instance.balance() should be (initialFunds - instance.valueSentFromMe(tx))
+  }
+
+  it must "block funds in create multisign transaction" in new Fixture {
+    val tx = instance.blockMultisignFunds(Seq(keyPair, someoneElsekeyPair), 1.BTC)
+    instance.value(tx) should be (-1.BTC)
     instance.balance() should be (initialFunds - instance.valueSentFromMe(tx))
   }
 
   it must "release funds previously blocked on create send" in new Fixture {
-    val tx = instance.blockFunds(someoneElse, 1.BTC)
+    val tx = instance.blockFunds(someoneElseAddress, 1.BTC)
     instance.releaseFunds(tx)
     instance.balance() should be (initialFunds)
   }
 
   it must "block funds after release" in new Fixture {
-    val tx1 = instance.blockFunds(someoneElse, 1.BTC)
+    val tx1 = instance.blockFunds(someoneElseAddress, 1.BTC)
     instance.releaseFunds(tx1)
-    val tx2 = instance.blockFunds(someoneElse, initialFunds)
+    val tx2 = instance.blockFunds(someoneElseAddress, initialFunds)
     instance.balance() should be (0.BTC)
+  }
+
+  it must "consider change funds after tx is broadcasted" in new Fixture {
+    val tx = instance.blockFunds(someoneElseAddress, 1.BTC)
+    sendToBlockChain(tx)
+    instance.balance() should be (initialFunds - 1.BTC)
+  }
+
+  it must "support a period of arbitrary spent or release operations" in new Fixture {
+    var expectedBalance = initialFunds
+    anArbitraryNumberOfTimes {
+      val tx = instance.blockFunds(someoneElseAddress, 0.1.BTC)
+      headsOrTails {
+        case `heads` =>
+          sendToBlockChain(tx)
+          expectedBalance += instance.value(tx)
+        case `tails` =>
+          instance.releaseFunds(tx)
+      }
+    }
+    instance.balance() should be (expectedBalance)
   }
 
   trait Fixture {
     val keyPair = new KeyPair
-    val someoneElse = new KeyPair().toAddress(network)
+    val someoneElsekeyPair = new KeyPair
+    val someoneElseAddress = someoneElsekeyPair.toAddress(network)
     val instance = createWallet(keyPair, 10.BTC)
     val initialFunds = instance.balance()
+
+    val heads = true
+    val tails = false
+
+    def anArbitraryNumberOfTimes(action: => Unit): Unit = {
+      for (_ <- 1 to 30) { action }
+    }
+
+    def headsOrTails(action: PartialFunction[Boolean, Unit]): Unit = {
+      action(Random.nextBoolean())
+    }
   }
 }
